d5538d4f68a46df68ce3f7d573012c968d0a0821
Andreas Ekeroot <andeke@gmail.com>
2025-01-24 21:12:50 +0100

    First threadbare prototype
    \o/ It has a cinnamon bun! \o/

---
+++ player.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+const GRAVITY = 200.0
+const WALK_SPEED = 200
+
+func _physics_process(delta):
+	velocity.y += delta * GRAVITY
+
+	if Input.is_action_pressed("ui_left"):
+		velocity.x = -WALK_SPEED
+	elif Input.is_action_pressed("ui_right"):
+		velocity.x =  WALK_SPEED
+	else:
+		velocity.x = 0
+
+	# "move_and_slide" already takes delta time into account.
+	move_and_slide()
