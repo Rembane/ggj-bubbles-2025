dccba44538f29c36d5a2834d6b670e30c2b606d0
Andreas Ekeroot <andeke@gmail.com>
2025-01-25 15:01:43 +0100

    Let the bubble stick to things
    Add a button-press to make it possible to stick the bubble to walls,
    floors, ceilings, etc.
    
    There's also quite a bit of weird stuff going on in the project file, I
    have no idea if that's relevant for this work.

--- player.gd
+++ player.gd
@@ -3,21 +3,28 @@ extends CharacterBody2D
 const GRAVITY = 200.0
 const WALK_SPEED = 2000
 const BORINGNESS = 0.8
-
-func _ready() -> void:
-	velocity = Vector2()
+var sticking_to_surface = false
 
 func _physics_process(delta):
-	velocity += Input.get_vector("move_left", "move_right", "move_up", "move_down") * WALK_SPEED * delta
-	velocity.y += GRAVITY * delta
+	if not sticking_to_surface:
+		velocity += Input.get_vector("move_left", "move_right", "move_up", "move_down") * WALK_SPEED * delta
 
 	var kinematic_collision = move_and_collide(velocity * delta)
 	if kinematic_collision:
-		velocity = velocity.bounce(kinematic_collision.get_normal()) * BORINGNESS
+		if sticking_to_surface:
+			velocity = Vector2.ZERO
+		else:
+			velocity = velocity.bounce(kinematic_collision.get_normal()) * BORINGNESS
 	else:
-		velocity.y += delta * GRAVITY
+		if not sticking_to_surface:
+			velocity.y += delta * GRAVITY
 
 func _input(event):
 	if event.is_action_pressed("reset"):
 		position = Vector2.ZERO
 		velocity = Vector2.ZERO
+
+	if Input.is_action_pressed("stick"):
+		sticking_to_surface = true
+	if event.is_action_released("stick"):
+		sticking_to_surface = false
