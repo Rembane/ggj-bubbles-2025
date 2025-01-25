96ba18e3742f2b3532b17c51fce95e60b4c48d12
Andreas Ekeroot <andeke@gmail.com>
2025-01-25 16:14:54 +0100

    Here, have a horrible state machine!
    I think I might have to do this in two steps, but who knows! :D

--- player.gd
+++ player.gd
@@ -1,30 +1,47 @@
 extends CharacterBody2D
 
+enum {NO_STICK, INTENT_TO_STICK, STICKING_TO_SURFACE, INTENT_TO_UNSTICK}
 const GRAVITY = 200.0
 const WALK_SPEED = 2000
 const BORINGNESS = 0.8
-var sticking_to_surface = false
+var stickiness_state_machine = NO_STICK
 
 func _physics_process(delta):
-	if not sticking_to_surface:
-		velocity += Input.get_vector("move_left", "move_right", "move_up", "move_down") * WALK_SPEED * delta
+	if stickiness_state_machine != STICKING_TO_SURFACE:
+		velocity += Input.get_vector("move_left", "move_right", "move_up", "move_down") \
+				* WALK_SPEED * delta
 
 	var kinematic_collision = move_and_collide(velocity * delta)
 	if kinematic_collision:
-		if sticking_to_surface:
+		if stickiness_state_machine == INTENT_TO_STICK:
+			stickiness_state_machine = STICKING_TO_SURFACE
+		if stickiness_state_machine == STICKING_TO_SURFACE:
 			velocity = Vector2.ZERO
 		else:
 			velocity = velocity.bounce(kinematic_collision.get_normal()) * BORINGNESS
 	else:
-		if not sticking_to_surface:
+		if stickiness_state_machine != STICKING_TO_SURFACE:
 			velocity.y += delta * GRAVITY
 
 func _input(event):
 	if event.is_action_pressed("reset"):
 		position = Vector2.ZERO
 		velocity = Vector2.ZERO
+		stickiness_state_machine = NO_STICK
 
-	if Input.is_action_pressed("stick"):
-		sticking_to_surface = true
-	if event.is_action_released("stick"):
-		sticking_to_surface = false
+	if event.is_action_pressed("stick"):
+		match stickiness_state_machine:
+			NO_STICK:
+				stickiness_state_machine = INTENT_TO_STICK
+			INTENT_TO_UNSTICK:
+				stickiness_state_machine = INTENT_TO_STICK
+			_:
+				pass
+	elif event.is_action_released("stick"):
+		match stickiness_state_machine:
+			INTENT_TO_STICK:
+				stickiness_state_machine = INTENT_TO_UNSTICK
+			STICKING_TO_SURFACE:
+				stickiness_state_machine = INTENT_TO_UNSTICK
+			_:
+				pass
