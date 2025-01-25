8cfcfe13885b9d014ef6c4ccaa4ea0faea2a1e31
Andreas Ekeroot <andeke@gmail.com>
2025-01-25 11:23:57 +0100

    Rewrite bounce, because vectors <3

--- player.gd
+++ player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	velocity = Vector2()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -13,8 +13,6 @@ const GRAVITY = 200.0
 const WALK_SPEED = 200
 
 func _physics_process(delta):
-	velocity.y += delta * GRAVITY
-
 	if Input.is_action_pressed("ui_left"):
 		velocity.x = -WALK_SPEED
 	elif Input.is_action_pressed("ui_right"):
@@ -22,14 +20,8 @@ func _physics_process(delta):
 	else:
 		velocity.x = 0
 
-	var pre_velocity = velocity.length()
-
-	while true:
-		var kinematic_collision = move_and_collide(velocity * delta)
-		if not kinematic_collision:
-			break
-
-		velocity = kinematic_collision.get_remainder()
-		velocity.y *= -1
-
-	velocity = velocity.normalized() * pre_velocity
+	var kinematic_collision = move_and_collide(velocity * delta)
+	if kinematic_collision:
+		velocity = velocity.bounce(kinematic_collision.get_normal())
+	else:
+		velocity.y += delta * GRAVITY
