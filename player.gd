d9f6a0d7068c2a91666fbafbf9a9cd58b4f5e3cd
Mika Ivarsson <mika@baam.se>
2025-01-25 16:15:28 +0100

    Dash experimentation

--- player.gd
+++ player.gd
@@ -8,46 +8,57 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	$VelocityMeter.clear_points()
-	$VelocityMeter.add_point( Vector2(0, 0) )
-	$VelocityMeter.add_point( velocity )
-	$VelocityMeter.global_rotation = 0
+	$VelocityMeter.add_point(Vector2.ZERO)
+	$VelocityMeter.add_point(velocity)
 	
 	$InputDisplay.clear_points()
-	$InputDisplay.add_point( Vector2(0, 0) )
-	$InputDisplay.add_point( input * 100 )
-	$InputDisplay.global_rotation = 0
+	$InputDisplay.add_point(Vector2.ZERO)
+	$InputDisplay.add_point(input * 100)
 	
 const GRAVITY = 500.0
-const WALK_SPEED = 3000
-const DASH_SPEED = 1000
+const WALK_SPEED = 400
+const DASH_SPEED = 5000
 const BORINGNESS = 0.3
 var input = Vector2()
 var surface_normal = Vector2.UP
+var gravity_momentum = Vector2.ZERO
+var movement_momentum = Vector2.ZERO
 
 func _physics_process(delta):
 	input = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	velocity *= 0.99
-	
+	gravity_momentum *= 0.999 # Terminal Velocity
+	movement_momentum *= 0.85
 
 	var kinematic_collision = move_and_collide(velocity * delta)
 	if kinematic_collision:
 		surface_normal = kinematic_collision.get_normal()
-		velocity -= velocity.project(surface_normal) #Cancel movement in normal direction
-		var normal_input = input.project(surface_normal.orthogonal())
-		velocity += normal_input * WALK_SPEED * delta #Walk along plane
+		var offset = Vector2(0, $CollisionShape2D.shape.radius * 2)
+		$"../NormalDisplay".clear_points()
+		$"../NormalDisplay".add_point(kinematic_collision.get_position() + offset)
+		$"../NormalDisplay".add_point(kinematic_collision.get_position() + offset + surface_normal * 100)
 		
-		velocity *= 0.8 # Friction
-		velocity -= surface_normal * GRAVITY * delta # Sticky surfaces
-		
-		if kinematic_collision.get_collider().get_meta("danger", false):
+		if kinematic_collision.get_collider().get_meta("sticky", false):
+			velocity -= velocity.project(surface_normal) #Cancel movement in normal direction
+			var normal_input = input.project(surface_normal.orthogonal())
+			movement_momentum = normal_input * WALK_SPEED # Walk along plane
+			
+			gravity_momentum = -surface_normal * GRAVITY * delta # Sticky surfaces
+			
+		elif kinematic_collision.get_collider().get_meta("bouncy", false):
+			gravity_momentum = gravity_momentum.bounce(surface_normal) * 1.3
+			movement_momentum = movement_momentum.bounce(surface_normal) * 1.3
+		elif kinematic_collision.get_collider().get_meta("danger", false):
 			reset()
 	else:
-		velocity.y += GRAVITY * delta
-		velocity += input * (WALK_SPEED / 10) * delta #Walk along plane
+		gravity_momentum.y += GRAVITY * delta
+		movement_momentum += input * (WALK_SPEED / 10) * delta
+	
+	velocity = movement_momentum.limit_length(1000) + gravity_momentum
 
 func _input(event):
 	if event.is_action_pressed("dash"):
-		velocity = input.normalized() * DASH_SPEED
+		movement_momentum = input.normalized() * DASH_SPEED
+		gravity_momentum = Vector2.ZERO
 	elif event.is_action_pressed("reset"):
 		reset()
 
