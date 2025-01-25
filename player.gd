d25f3423855a27db2dfd42c20da30866c4826d2b
Andreas Ekeroot <andeke@gmail.com>
2025-01-25 18:41:02 +0100

    Make it possible to slide down on a wall by hitting X repeatedly
    The code is quite gnarly though. I'm not completely convinced that this
    was a good idea. :D

--- player.gd
+++ player.gd
@@ -5,6 +5,8 @@ const GRAVITY = 200.0
 const WALK_SPEED = 2000
 const BORINGNESS = 0.8
 var stickiness_state_machine = NO_STICK
+var last_collision = null
+var outer_collision_overlaps = null
 
 func _physics_process(delta):
 	if stickiness_state_machine != STICKING_TO_SURFACE:
@@ -17,10 +19,16 @@ func _physics_process(delta):
 			INTENT_TO_STICK, STICKING_TO_SURFACE:
 				stickiness_state_machine = STICKING_TO_SURFACE
 				velocity = Vector2.ZERO
+				last_collision = kinematic_collision.get_collider()
 			_:
 				velocity = velocity.bounce(kinematic_collision.get_normal()) \
 						* BORINGNESS
 	else:
+		if stickiness_state_machine in [STICKING_TO_SURFACE, INTENT_TO_STICK] \
+				and last_collision != null and outer_collision_overlaps == last_collision:
+			stickiness_state_machine = STICKING_TO_SURFACE
+			velocity = Vector2.ZERO
+
 		if stickiness_state_machine != STICKING_TO_SURFACE:
 			velocity.y += delta * GRAVITY
 
@@ -38,3 +46,11 @@ func _input(event):
 				pass
 	elif event.is_action_released("stick"):
 		stickiness_state_machine = NO_STICK
+
+func _on_outer_collision_area_body_entered(body: Node2D) -> void:
+	outer_collision_overlaps = body
+
+func _on_outer_collision_area_body_exited(body: Node2D) -> void:
+	if body == outer_collision_overlaps and outer_collision_overlaps == last_collision:
+		outer_collision_overlaps = null
+		last_collision = null
